dc5c48712abf0d03ff15c774d2158f0a8c283613
joshwidjaja <josh@widjaja.us>
2024-11-23 15:10:39 -0800

    slot struct setup

--- Assets/Scripts/Template.cs
+++ Assets/Scripts/Template.cs
@@ -2,8 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Template : MonoBehaviour
 {
+    enum Type {Head, Chest, Arm, Leg};
+    Slot[] slots;
+
+    struct Slot
+    {
+        Type type;
+        Card card;
+    }
     // Start is called before the first frame update
     void Start()
     {

fe634c6bf8212d8d7f3e037a4bfc479759e70cd6
joshwidjaja <josh@widjaja.us>
2024-11-25 19:37:44 -0800

    changed slots to dictionary layout

--- Assets/Scripts/Template.cs
+++ Assets/Scripts/Template.cs
@@ -5,18 +5,18 @@ using UnityEngine;
 
 public class Template : MonoBehaviour
 {
-    enum Type {Head, Chest, Arm, Leg};
-    Slot[] slots;
+    enum PartType {Head, Chest, Arm, Leg};
+    Dictionary<string, Card> slots = new Dictionary<string, Card>();
 
-    struct Slot
-    {
-        Type type;
-        Card card;
-    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        slots.Add("Head", null);
+        slots.Add("Chest", null);
+        slots.Add("ArmL", null);
+        slots.Add("ArmR", null);
+        slots.Add("LegL", null);
+        slots.Add("LegR", null);   
     }
 
     // Update is called once per frame

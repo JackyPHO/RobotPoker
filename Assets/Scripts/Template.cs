1ad9d81e072aa3dd065a60f5618bc5822ce70300
joshwidjaja <josh@widjaja.us>
2024-11-25 19:51:26 -0800

    additional functions before robot class

--- Assets/Scripts/Template.cs
+++ Assets/Scripts/Template.cs
@@ -6,11 +6,14 @@ using UnityEngine;
 public class Template : MonoBehaviour
 {
     enum PartType {Head, Chest, Arm, Leg};
-    Dictionary<string, Card> slots = new Dictionary<string, Card>();
+    Dictionary<string, Card> slots;
+    Robot robot;
 
     // Start is called before the first frame update
     void Start()
     {
+        robot = new Robot();
+        slots = new Dictionary<string, Card>();
         slots.Add("Head", null);
         slots.Add("Chest", null);
         slots.Add("ArmL", null);
@@ -18,6 +21,20 @@ public class Template : MonoBehaviour
         slots.Add("LegL", null);
         slots.Add("LegR", null);   
     }
+
+    private void TestCard(Card card, string slotTitle)
+    {
+        if (slotTitle.StartsWith(card.type))
+        {
+            // add new part to robot
+        }
+    }
+
+    void GetRobot()
+    {
+        return robot;
+    }
+
 
     // Update is called once per frame
     void Update()
